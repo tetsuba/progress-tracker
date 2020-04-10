f7a2fbcfba6945867e7b3929244ba7b26fe7fa57
Ryan Marchock <tetsuba@ryans-mbp.home>
2020-04-10 23:15:00 +0100

    created email templates and unit tests

--- src/server/utils/email/sendEmail.js
+++ src/server/utils/email/sendEmail.js
@@ -1,55 +1,63 @@
-// TODO:
-// - Create email template
-// - Create a none reply email
-// - Create token with 12 hours expire
-// - Create Route to confirm token
-
+const fs = require('fs');
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-    service: process.env.REACT_APP_NODE_MAIL_SERVICE,
-    auth: {
-        user: process.env.REACT_APP_NODE_MAIL_ADDRESS,
-        pass: process.env.REACT_APP_NODE_MAIL_ADDRESS_PASSWORD,
+let baseUrl = '';
+if (process.env.DEV || process.env.TEST) {
+    baseUrl = 'http://localhost:3000/'
+} else {
+    // TODO: Production URL
+    baseUrl = ''
+}
+
+const mailOptions = {
+    confirmEmail: {
+        emailTemplateName: 'confirmEmailTemplate',
+        pathName: 'confirm',
+        subject: 'Progress tracker confirm email address.',
+    },
+    resetPassword: {
+        emailTemplateName: 'resetPasswordTemplate',
+        pathName: 'reset',
+        subject: 'Progress tracker password reset request.',
     }
-});
+}
+
+function getEmailMailOptions(name) {
+    const option = mailOptions[name];
+    const path = __dirname + `/template/${option.emailTemplateName}.html`
+    const fsOptions = { encoding: 'utf-8' };
+    const url = `${baseUrl}${option.pathName}/{{token}}`;
+    const html = fs.readFileSync(path, fsOptions);
+    return {
+        html: html.replace('{{href}}', url),
+        subject: option.subject,
+    }
+}
+
+async function sendMail(token, { html, subject }) {
+    const transporter = nodemailer.createTransport({
+        service: process.env.REACT_APP_NODE_MAIL_SERVICE,
+        auth: {
+            user: process.env.REACT_APP_NODE_MAIL_ADDRESS,
+            pass: process.env.REACT_APP_NODE_MAIL_ADDRESS_PASSWORD,
+        }
+    });
+
+    const mailOptions = {
+        from: process.env.REACT_APP_NODE_MAIL_ADDRESS,
+        to: 'tetsubalimited@gmail.com',
+        subject: subject,
+        html: html.replace('{{token}}', token),
+    };
+
+    try {
+        return await transporter.sendMail(mailOptions)
+    } catch(e) {
+        return e
+    }
+}
 
 module.exports = {
-    sendMail: (token, template) => {
-
-        const emailTemplate = {
-            confirmEmail: {
-                html: `
-                    <h1>Progress Tracker</h1>
-                    <p>Click on the link below to confirm your email address</p>
-                    <a href="http://localhost:3000/confirm/${encodeURIComponent(token)}">Confirm</a>
-                `,
-                subject: 'Progress tracker confirm your email'
-            },
-            resetPassword: {
-                html: `
-                    <h1>Progress Tracker</h1>
-                    <p>Click on the link below to reset your password</p>
-                    <a href="http://localhost:3000/reset/${encodeURIComponent(token)}">Confirm</a>
-                `,
-                subject: 'Progress tracker confirm your email'
-            }
-        };
-
-        const mailOptions = {
-            from: process.env.REACT_APP_NODE_MAIL_ADDRESS,
-            to: 'tetsubalimited@gmail.com',
-            subject: emailTemplate[template].subject,
-            html: emailTemplate[template].html,
-        };
-
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
-    }
+    sendMail,
+    getEmailMailOptions
 };
-
