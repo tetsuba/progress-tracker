133fc548cf1043899c22738824cb36ed10510ae5
Ryan Marchock <tetsuba@ryans-mbp.home>
2020-05-12 21:21:39 +0100

    reworked route

--- src/client/routes/Routes.js
+++ src/client/routes/Routes.js
@@ -15,29 +15,23 @@ import UnProtectedRoute from './UnProtectedRoute'
 import CourseRoutes from '../screens/Course/routes'
 import ConfirmAccount from '../screens/ConfirmAccount/ConfirmAccount'
 import ResetPassword from '../screens/ResetPassword/ResetPassword'
-import ABC from '../screens/Course/ABC/ABC'
-import AbcUnit from '../screens/Course/ABC/AbcUnit'
 
 export default function Routes() {
   return (
     <Switch>
       <ProtectedRoute path="/" exact component={Home} />
-      <ProtectedRoute path="/students" component={Students} />
-      <ProtectedRoute path="/myAccount" component={MyAccount} />
-      <ProtectedRoute path="/student/:id" exact component={Student} />
       {
         // TODO: Do I need a link to courses?
         /*<ProtectedRoute path="/student/:id/courses" component={} />*/
       }
-      <ProtectedRoute strict path="/student/:id/course/ABC" component={ABC} />
-      <ProtectedRoute
-        path="/student/:id/course/ABC/:unit"
-        component={AbcUnit}
-      />
       <UnProtectedRoute path="/login" Component={Login} />
       <Route path="/register" component={Register} />
       <Route path="/confirm/:token" component={ConfirmAccount} />
       <Route path="/reset/:token" component={ResetPassword} />
+
+      <ProtectedRoute path="/myAccount" component={MyAccount} />
+      <ProtectedRoute path="/students" component={Students} />
+      <ProtectedRoute path="/student/:id" exact component={Student} />
       <CourseRoutes />
     </Switch>
   )
