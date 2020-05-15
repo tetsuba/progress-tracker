cd55029419733d693f4266b9c761967b89dd2578
Ryan Marchock <tetsuba@ryans-mbp.home>
2020-05-16 00:47:04 +0100

    PROG-69: fixed flow errors

--- src/client/routes/ProtectedRoute.js
+++ src/client/routes/ProtectedRoute.js
@@ -1,8 +1,24 @@
 import React, { useContext } from 'react'
+// $FlowFixMe - Investigate how to fix context flow issue
 import { AuthenticatedContext } from '../context/AuthenticatedContext'
 import { Redirect, Route } from 'react-router-dom'
 
-export default function ProtectedRoute(props) {
+// TYPES
+import type { ComponentType, Node } from 'react'
+import type { ContextRouter, LocationShape } from 'react-router-dom'
+
+type Props = {|
+  component?: ComponentType<*>,
+  render?: (router: ContextRouter) => Node,
+  children?: ComponentType<ContextRouter> | Node,
+  path?: string | Array<string>,
+  exact?: boolean,
+  strict?: boolean,
+  location?: LocationShape,
+  sensitive?: boolean,
+|}
+
+export default function ProtectedRoute(props: Props) {
   console.log('<ProtectedRoute>: ', props)
   const { authenticated } = useContext(AuthenticatedContext)
 
